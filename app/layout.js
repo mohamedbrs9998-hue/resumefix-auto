eeab6e0ff09004b8dcd1d190cf223704fa45e616
mac <mac@macs-MacBook-Air.local>
2026-04-14 02:30:20 +0400

    Fix layout.js completely

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -22,8 +20,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={${geistSans.variable} ${geistMono.variable}}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={${geistSans.className} ${geistMono.className}}>
+        {children}
+      </body>
     </html>
   );
 }
